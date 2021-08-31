459f54dea603578e425a03d4458d92bdae678eb6
Jason Antonino <jeff_antonino@yahoo.com>
2021-08-31 17:08:07 +0100

    Modified __init__.py by adding app.config which sets references an SQLite database (local/inbuilt). This needs to be changed to MySQL remote database for production.

--- application/__init__.py
+++ application/__init__.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-db = SQLAlchemy(app)
+#Creates a local database (*****NEEDS TO BE CHANGED TO AWS MYSQL DATABASE FOR PRODUCTION*****)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"     
+
+db = SQLAlchemy(app)        #Creates the database object
 
 from application import routes
