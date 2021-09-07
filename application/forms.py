a10f30b20e200706e447083b72ac57ad58716126
Jason Antonino <jeff_antonino@yahoo.com>
2021-09-07 12:32:09 +0100

    Implemented Unit Tests for the Views, CRUD Operations and ValidationError raises. Also added fixes to some form input errors (adding team that already exists in the database).

--- application/forms.py
+++ application/forms.py
@@ -3,11 +3,18 @@ from flask import Flask, render_template, request                       #Flask i
 from flask_wtf import FlaskForm                                         #Imports form from flask_wtf
 from wtforms import StringField, IntegerField, SelectField, SubmitField              #Imports used fields for the form
 from wtforms.validators import DataRequired, Length, ValidationError    #Imports validators for the form
+from application.models import Teams, Players
+
+def validate_Team_name(self, field):
+    allTeams = Teams.query.all()
+    for team in allTeams:
+        if field.data == team.team_name:
+            raise ValidationError("The team already exists.")
 
 #Form for adding a new Team record
 class TeamForm(FlaskForm):
     team_name = StringField('Team Name', 
-        validators=[DataRequired(), Length(min=1, max=50)])   #Collects team name
+        validators=[DataRequired(), Length(min=1, max=50), validate_Team_name])   #Collects team name
     
     team_manager = StringField('Manager Name',
         validators=[DataRequired(), Length(min=1, max=120)])  #Collects manager name
@@ -17,22 +24,19 @@ class TeamForm(FlaskForm):
     
     submit = SubmitField('Submit')                          #Submit button
 
-#Form to select which team will be updated
+#Form for selecting a team to update
 class UpdateTeamForm(FlaskForm):
     team_name = SelectField('Select Team to update', choices=[],
         validators=[DataRequired()])
 
     submit = SubmitField('Edit Team')
 
-
+#Form for selecting a team to delete
 class DeleteTeamForm(FlaskForm):
     team_id = SelectField('Select Team to delete', choices=[],
         validators=[DataRequired()])
 
     submit = SubmitField('Delete Team')
-
-
-
 
 #Form for adding a new Player record
 class PlayerForm(FlaskForm):
@@ -50,14 +54,14 @@ class PlayerForm(FlaskForm):
         
     submit = SubmitField('Submit')                          #Submit button
 
-
+#Form for selecting a player to update
 class UpdatePlayerForm(FlaskForm):
     player_id = SelectField('Select player to update', choices=[],
         validators=[DataRequired()])
 
     submit = SubmitField('Edit Player')
 
-
+#Form for selecting a player to delete
 class DeletePlayerForm(FlaskForm):
     player_id = SelectField('Select player to delete', choices=[],
         validators=[DataRequired()])
