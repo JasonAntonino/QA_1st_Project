0d9d500fa85db4adc5d4c139b7fe7c6f2b9b6fcd
Jason Antonino <jeff_antonino@yahoo.com>
2021-09-08 14:33:51 +0100

    Changed the app configs for the database uri and secret key to use the environment variables. Still need to create a remote database to be used for the final version.

--- application/__init__.py
+++ application/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
 #Creates a local database (*****NEEDS TO BE CHANGED TO AWS MYSQL DATABASE FOR PRODUCTION*****)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"     
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")     
 
 #Creates a secret key which is used by the forms (*****NEEDS TO BE CHANGED AND PUT IN AN ENVIRONMENT VARIABLE*****)
-app.config['SECRET_KEY'] = '1234567abc'
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
 db = SQLAlchemy(app)        #Creates the database object
 
